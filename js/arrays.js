8c37196d7e99913b78ddff02673de8453740af53
Nikola <nikolaviki@homail.com>
2020-09-11 09:58:37 +0200

    #LAGJ-25 finising task

--- js/arrays.js
+++ js/arrays.js
@@ -15,6 +15,16 @@ const loop = (colurs, type)=> {
 loop(colurs, 'for');
 loop(colurs, 'forEach');
 
+/**
+ * 
+ * rotate an array to the right by a certain number of steps 
+ */
+    const rotateArray = (steps, arr)=>{ 
+        const newArr = arr.splice(steps);
+
+         return [...arr.reverse(), ...newArr];
+    };
+    console.log(rotateArray(3,[0,1,2,3,4,5,6]))
 
 /**
  * 
@@ -23,6 +33,21 @@ loop(colurs, 'forEach');
 const numbersForReduce = [1, 2, 3, 4, 5]
 const sumArr = (array)=> array.reduce((acc, element)=> acc + element, 50)
 console.log(sumArr(numbersForReduce));
+
+/**
+ * 
+ * creating array  
+ */
+
+
+ const createArr = (num) =>{
+
+     let array = Array.from(Array(num + 1), (x, index)=> index++);
+     let [...array2] = array;
+     return [...array2, ...array.reverse()]
+
+ }
+ console.log(createArr(5));
 
 /**
  * zoo task
